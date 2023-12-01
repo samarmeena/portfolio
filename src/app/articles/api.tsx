03762e891685aab8427f2746728dc6097ee38dfe
samarmeena <sumarmeena@gmail.com>
2023-12-01 16:40:17 +0530

    refactor: remove unwanted console log

--- src/app/articles/api.tsx
+++ src/app/articles/api.tsx
@@ -4,7 +4,6 @@ export async function GetArticles() {
   if (!process.env.DEV_TO_API_KEY) {
     throw Error("env DEV_TO_API_KEY does not exist");
   }
-  console.log(process.env.DEV_TO_API_KEY);
 
   const res = await fetch(
     "https://dev.to/api/articles/me/published?per_page=6",
