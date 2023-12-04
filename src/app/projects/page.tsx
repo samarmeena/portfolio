e5e009c3091347c0b3a0494de6d26fb6aded8b83
samarmeena <sumarmeena@gmail.com>
2023-12-04 10:48:42 +0530

    chore: prettier

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,7 +7,7 @@ const ProjectsPage: React.FC = () => {
   return (
     <>
       <h3>{"Stuff I've Built So Far"}</h3>
-      <div className="grid md:grid-cols-[repeat(auto-fit,minmax(400px,1fr))] my-6 gap-6">
+      <div className="my-6 grid gap-6 md:grid-cols-[repeat(auto-fit,minmax(400px,1fr))]">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
